36cc82b34986352dcfba07aa1e62bb27d42c3aa4
Alex <alexnoir03@gmail.com>
2020-08-18 01:07:16 +0100

    Conversation "simulator" testing

---
+++ Disco/User.cs
@@ -0,0 +1,9 @@
+﻿namespace Disco
+{
+    internal struct User
+    {
+        public string Name { get; set; } 
+        public string BasedOn { get; set; }
+        public string Avatar { get; set; }
+    }
+}
